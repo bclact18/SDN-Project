20adb04f12bd34759e9695924cc3a7dd06973702
misterpurple98 <misterpurple98@gmail.com>
2024-02-29 20:02:41 -0800

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 @echo off
 python -m venv venv
-call venv/Scripts/activate
+call venv\Scripts\activate
 pip install -r requirements.txt
-echo Requirements installed, launch run.bat to run
+echo Requirements installed, run run.bat to start
 pause
