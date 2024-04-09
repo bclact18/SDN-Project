d764f2b75f97efaf940b9b134889b4240295574e
misterpurple98 <misterpurple98@gmail.com>
2024-04-08 23:09:36 -0700

    Removed test code Added argument parsing and decorator registry TODO: Use ARGV instead of counting arguments TODO: Begin implementing flag functionality

--- main.py
+++ main.py
@@ -1,13 +1,50 @@
-import tensorflow
-import module
 import sys
 
-def inVenv():
-    return sys.prefix != sys.base_prefix
+# Registry and decorator to add functions called with --args without explicitly coding them
+# Improper number of arguments or no arguments must be errored within the function and caught by the try block at function call
+registry = {}
+def register(callstring):
+    def funcRegister(func):
+        registry[callstring] = func
+        return func
+    return funcRegister
 
+@register("--SNIP")
+def controlTest(arguments):
+    print("I'm sniffing now")
+    print(f"I was given {arguments[0]} arguments")
+    print(f"The argument list is {arguments[1]}")
+
+@register("--CTIP")
+def controlTest(arguments):
+    print("I'm controlling now")
+    print(f"I was given {arguments[0]} arguments")
+    print(f"The argument list is {arguments[1]}")
 
 if __name__ == "__main__":
-    if inVenv():
-        print('I am main in a venv')
-    print(f'This is the main file, tensorflow version {tensorflow.__version__}')
-    module.printpygments()
+    argumentGroups = {}
+    argPrev = ""
+
+    for arg in sys.argv[1:]:
+        if (arg[0:2] == "--"):
+            argumentGroups[arg] = [0, []]
+            argPrev = arg
+        else:
+            argumentGroups[argPrev][1].append(arg)
+            argumentGroups[argPrev][0] += 1
+    
+    if "--CTIP" not in argumentGroups:
+        raise Exception("Controller IP must be specified with --CTIP [IP]")
+
+    if "--SNIP" not in argumentGroups:
+        raise Exception("Interface to sniff must be specified with --SNIP [IP]")
+    
+    for flag, params in argumentGroups.items():
+        try:
+            registry[flag](params)
+        except KeyError as err:
+            raise Exception(f"Flag \"{flag}\" is not implemented") from err
+        except Exception as err:
+            raise err
+    
+    print(argumentGroups)
