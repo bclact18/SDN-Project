098cac25aa6a6c79c6f7a35db19e861d3764fd14
misterpurple98 <misterpurple98@gmail.com>
2024-04-23 19:23:34 -0700

    Added PUT and DELETE methods for flow control

--- REST_API.py
+++ REST_API.py
@@ -40,14 +40,56 @@ class RestFramework():
     # Function to issue HTTP DELETE request
     def delete_request(self, endpoint):
         url = f"{self.base_url}{endpoint}"
-        response = requests.delete(url)
+        response = requests.delete(url, auth=self.auth)
         return response.status_code
     
     def get_topo(self):
         response = self.get_request(self.topoEnd)
         return [response.status_code, response.json()]
+    
+    def put_drop(self, switch, tableId, flowName, flowId, ethAddr, priority):
+        data = self.make_drop_request(tableId, flowName, flowId, ethAddr, priority)
+        return self.put_request(f"{self.cnodeEnd}node/{switch}/table/{tableId}/flow/{flowId}", data)
+
+    def delete_flow(self, switch, tableId, flowId):
+        return self.delete_request(f"{self.cnodeEnd}node/{switch}/table/{tableId}/flow/{flowId}")
+
+    def make_drop_request(self, tableId, flowName, flowId, ethAddr, priority):
+        return {'flow':{
+                'strict': 'true',
+                'instructions':{
+                    "instruction": {
+                    "order": "0",
+                    "apply-actions": {
+                        "action": {
+                            "order": "0",
+                            "drop-action": {}
+                            }
+                        }
+                    }
+                },
+                "table_id": tableId,
+                "id": flowId,
+                "cookie_mask": "0",
+                "match": {
+                    "ethernet-match": {
+                        "ethernet-source": {
+                            "address": ethAddr
+                        }
+                    }
+                },
+                "hard-timeout": "300",
+                "cookie": "1234",
+                "idle-timeout": "600",
+                "flow-name": flowName,
+                "priority": priority
+            }}
+
 
 # Example usage
 if __name__ == "__main__":
-    testCont = RestFramework('192.168.56.106', 'admin', 'admin')
-    print(testCont.get_topo())
+    testCont = RestFramework('172.17.42.205', 'admin', 'admin')
+    #print(testCont.get_topo())
+
+    print(True if testCont.put_drop('openflow:1', '0', 'test', '1212', '00:00:00:00:00:01', '20').status_code == 201 else False)
+    print(testCont.delete_flow('openflow:1', '0', '1212'))
